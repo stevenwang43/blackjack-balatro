84f0fd0a722e748971f1a1a16443519fddf070bc
espageti <darreeb.lin@gmail.com>
2025-04-26 00:41:36 -0500

    Implement standing and hitting, and Dealer

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using System.Collections;
 
 public class GameManager : MonoBehaviour
 {
@@ -7,6 +8,16 @@ public class GameManager : MonoBehaviour
     public HandManager playerHand;
     public DeckManager playerDeck;
     public UIManager uiManager;
+    public enum GameState
+    {
+        PlayerTurn,
+        DealerTurn,
+        RoundOver
+    }
+    public GameState state = GameState.PlayerTurn;
+
+
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -25,14 +36,52 @@ public class GameManager : MonoBehaviour
         uiManager.UpdateUI();
         if(playerHand.GetTotal() > 21)
         {
+            state = GameState.RoundOver;
             return;
         }
+        state = GameState.DealerTurn;
+        StartCoroutine(DealerTurnWithDelay(false));
+        state = GameState.PlayerTurn;
+
+    }
+
+    public void PlayerStand()
+    {
+        state = GameState.DealerTurn;
+        StartCoroutine(DealerTurnWithDelay(true));
+
+
+
     }
 
     void Lose()
     {
 
     }
+
+    IEnumerator DealerTurnWithDelay(bool fullTurn)
+    {
+        yield return new WaitForSeconds(0.5f); // delay before starting
+
+        if (fullTurn)
+        {
+            int result = dealer.TakeTurn();
+            uiManager.UpdateUI();
+            while (result == 1)
+            {
+                yield return new WaitForSeconds(0.5f); // delay between dealer actions
+                result = dealer.TakeTurn();
+                uiManager.UpdateUI();
+            }
+            state = GameState.RoundOver;
+        }
+        else
+        {
+            dealer.TakeTurn(); // just one action
+            state = GameState.PlayerTurn;
+        }
+    }
+
 
 
 
--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -4,18 +4,25 @@ using TMPro;
 
 public class UIManager : MonoBehaviour
 {
-    public Button drawCardButton;
+    public Button hitButton;
+    public Button standButton;
     public TMP_Text playerHandTotal;
+    public TMP_Text dealerHandTotal;
     public HandManager playerHand;
     public GameManager gameManager;
+    public Dealer dealer;
 
     void Start()
     {
-        drawCardButton.onClick.AddListener(() => gameManager.PlayerDraw());
+        hitButton.onClick.AddListener(() => gameManager.PlayerDraw());
+        standButton.onClick.AddListener(() => gameManager.PlayerStand());
+        UpdateUI();
     }
 
     public void UpdateUI()
     {
         playerHandTotal.text = playerHand.GetTotal().ToString();
+        dealerHandTotal.text = dealer.HandTotal().ToString();
+
     }
 }
