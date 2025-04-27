78d4883641a3f84e6800ea9141c6518e209878e0
espageti <darreeb.lin@gmail.com>
2025-04-27 02:05:04 -0500

    aces

--- Assets/Scripts/HandManager.cs
+++ Assets/Scripts/HandManager.cs
@@ -2,6 +2,7 @@ using UnityEngine;
 using Blackjack;
 using System.Collections.Generic;
 using System;
+using System.Linq;
 
 public class HandManager : MonoBehaviour
 {
@@ -31,27 +32,76 @@ public class HandManager : MonoBehaviour
 
     public int GetTotal()
     {
-        float total = 0;
-        
-        // Get card value multiplier if ModifierManager exists
+        // Get modifiers if they exist
         float cardValueMultiplier = 1.0f;
+        float blackjackThreshold = 21f;
+        
         if (ModifierManager.Instance != null)
         {
             cardValueMultiplier = ModifierManager.Instance.GetModifierValue(ModifierManager.ModifierType.CardValueMultiplier);
+            blackjackThreshold = ModifierManager.Instance.GetModifierValue(ModifierManager.ModifierType.BlackjackThreshold);
         }
         
+        // Start with a list containing just 0 as the base score
+        List<float> possibleScores = new List<float> { 0 };
+        
+        // Process each card to generate all possible scores
         foreach (Card c in cardsData)
         {
-            total += c.value * cardValueMultiplier;
+            int newPossibleScoresCount = possibleScores.Count;
+            
+            // If this is an ace, it has two possible values (1 or 11)
+            if (c.displayValue == "A")
+            {
+                // For each existing possible score, add two new possibilities: +1 and +11
+                for (int i = 0; i < newPossibleScoresCount; i++)
+                {
+                    float currentScore = possibleScores[i];
+                    
+                    // Add ace as value 1
+                    possibleScores[i] = currentScore + (1 * cardValueMultiplier);
+                    
+                    // Add a new possibility with ace as value 11
+                    possibleScores.Add(currentScore + (11 * cardValueMultiplier));
+                }
+            }
+            else
+            {
+                // For regular cards, just add their value to all possible scores
+                for (int i = 0; i < newPossibleScoresCount; i++)
+                {
+                    possibleScores[i] += c.value * cardValueMultiplier;
+                }
+            }
         }
         
-        // Apply hand total bonus if ModifierManager exists
+        // Apply hand total bonus to all possible scores if ModifierManager exists
         if (ModifierManager.Instance != null)
         {
-            total += ModifierManager.Instance.GetModifierValue(ModifierManager.ModifierType.HandTotalBonus);
+            float bonus = ModifierManager.Instance.GetModifierValue(ModifierManager.ModifierType.HandTotalBonus);
+            for (int i = 0; i < possibleScores.Count; i++)
+            {
+                possibleScores[i] += bonus;
+            }
         }
         
-        return Mathf.RoundToInt(total);
+        // Find the best score (highest score that's not over blackjackThreshold)
+        float bestScore = 0;
+        foreach (float score in possibleScores)
+        {
+            if (score <= blackjackThreshold && score > bestScore)
+            {
+                bestScore = score;
+            }
+        }
+        
+        // If we don't have any valid scores under the threshold, take the lowest score
+        if (bestScore == 0 && possibleScores.Count > 0)
+        {
+            bestScore = possibleScores.Min();
+        }
+        
+        return Mathf.RoundToInt(bestScore);
     }
 
     public void AddCardToHand(Card cardData)
